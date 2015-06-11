a3c58d5406912de6c71a16f961b8c561479cd8ef
Priyatam Mudivarti <priyatam@gmail.com>
2015-06-11 17:02:53 +0900

    added dynamic vars

--- src/closures.clj
+++ src/closures.clj
@@ -4,3 +4,12 @@
   (let [counter (atom 0)]
     (fn []
       (do (swap! counter inc) @counter))))
+
+(def ^:dynamic *evalme* 10)
+
+;; Dynamic Binding
+
+(binding [*evalme* 20]
+  (str "hello" *evalme*))
+
+(str "hello" *evalme*)
