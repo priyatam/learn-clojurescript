2d6f10e892006b6cccc166ced97bba98e4fe00c7
Priyatam Mudivarti <priyatam@gmail.com>
2015-06-11 17:24:12 +0900

    added loops

---
+++ src/loops.clj
@@ -0,0 +1,23 @@
+(ns loops)
+
+;;;;;;;;
+;; Simple Loops
+
+(defn countdown [n]
+  (if-not (zero? n)
+    (do
+      (if (= 0 (rem n 10))
+        (println  "countdown: " n))
+      (recur (dec n)))))
+
+;; (countdown 100)
+
+;;;;;;;;;;;;;;;;;;;;;
+;; List Comprehensions
+
+(defn prime? [n]
+  (not-any? zero? (map #(rem n %) (range 2 n))))
+
+(for [x (range 3 17 2) :when (prime? x)
+      y (range 3 17 2) :when (prime? y)]
+  [x y])
