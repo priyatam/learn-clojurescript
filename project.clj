26101d5fcc4286504f1c31b5ae5a1f8de6780b8f
Priyatam Mudivarti <priyatam@gmail.com>
2015-07-25 00:42:32 -0700

    add garden

--- project.clj
+++ project.clj
@@ -10,7 +10,8 @@
                  [org.clojure/clojurescript "0.0-3367"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.cognitect/transit-cljs "0.8.220"]
-                 [com.cognitect/transit-clj "0.8.275"]]
+                 [com.cognitect/transit-clj "0.8.275"]
+                 [garden "1.2.5"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
                                   [priyatam/replify "0.2.2"]]}}
   :plugins [[cider/cider-nrepl "0.9.1"]
---
+++ src/thinking/css.cljc
@@ -0,0 +1,12 @@
+(ns thinking.css
+  (:require
+   [garden.core :refer [css]]))
+
+(css [:body {:font-size "16px"}])
+
+(defn clearfix [clazz]
+  [clazz {:*zoom 1}
+   [:&:before :&:after {:display "table"
+                        :content " "
+                        :line-height 0}]
+   [:&:after {:clear "both"}]])
