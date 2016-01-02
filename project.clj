e52b504c002b5ec03fb2625d4ed9014696aac73f
Priyatam Mudivarti <priyatam@gmail.com>
2016-01-02 10:26:43 -0800

    bump versions

--- project.clj
+++ project.clj
@@ -1,18 +1,17 @@
-(defproject thinking-clojurescript "0.2.0"
+(defproject thinking-clojurescript "0.3.0"
   :description "Thinking Clojurescript, a learning series"
   :url "https://github.com/priyatam/thinking-clojurescript"
   :scm {:name "git" :url "https://github.com/priyatam/thinking-clojurescript"}
   :min-lein-version "2.5.0"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ["-Xms1g" "-server"]
+  :jvm-opts ["-Xmx512g" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.cognitect/transit-cljs "0.8.220"]
-                 [com.cognitect/transit-clj "0.8.275"]
-                 [garden "1.2.5"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/core.async "0.2.374"]
+                 [com.cognitect/transit-cljs "0.8.237"]
+                 [com.cognitect/transit-clj "0.8.285"]
+                 [garden "1.3.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.11"]
                                   [priyatam/replify "0.2.2"]]}}
-  :plugins [[cider/cider-nrepl "0.9.1"]
-            [lein-marginalia "0.8.0"]])
+  :plugins [[lein-marginalia "0.8.0"]])
