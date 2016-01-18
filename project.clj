bff923643e8a835d22a6289bd3d736d5a018cc09
Priyatam Mudivarti <priyatam@gmail.com>
2016-01-18 12:44:16 -0800

    up deps

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
   :jvm-opts ["-Xmx512g" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.374"]
                  [com.cognitect/transit-cljs "0.8.237"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [com.stuartsierra/component "0.3.1"]
                  [garden "1.3.0"]
                  [hipo "0.5.2"]]
-  :profiles {:dev {:dependencies [[priyatam/replify "0.2.2"]]}}
+  :profiles {:dev {:dependencies [[facjure/replify "0.3.0"]]}}
   :plugins [[lein-marginalia "0.8.0"]])
