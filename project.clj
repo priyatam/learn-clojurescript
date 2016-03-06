72ee02910bf7b6a7368470f828f5dd593e2017aa
Priyatam Mudivarti <priyatam@gmail.com>
2016-03-05 16:02:18 -0800

    up versions, cleanup config

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject learn-clojurescript "0.3.1-SNAPSHOT"
-  :description "Learn Isomorphic Clojurescript, a thinking guide on the REPL"
+  :description "Learn Isomorphic Clojurescript, on the REPL"
   :url "https://github.com/priyatam/learn-clojurescript"
   :scm {:name "git" :url "https://github.com/priyatam/learn-clojurescript"}
-  :min-lein-version "2.5.0"
-  :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ["-Xmx128m" "-server"]
+  :min-lein-version "2.6.0"
+  :license {:name "MIT License" :url "https://opensource.org/licenses/MIT"}
+  :jvm-opts ["-Xmx256m" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
@@ -13,9 +13,10 @@
                  [com.cognitect/transit-cljs "0.8.237"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [com.stuartsierra/component "0.3.1"]
-                 [garden "1.3.1"]
-                 [hipo "0.5.2"]
-                 [jayq "2.5.4"]]
-  :profiles {:dev {:dependencies [[facjure/replify "0.3.0"]]}}
+                 [garden "1.3.1"]]
+  :profiles {:dev  {:dependencies [[facjure/replify "0.3.0"]]}
+             :demo {:source-paths ["demo"]
+                    :dependencies [[hipo "0.5.2"]
+                                   [jayq "2.5.4"]]}}
   :plugins [[lein-marginalia "0.8.0"]]
   :main replify)
