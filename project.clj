e54cde95b5dbf9c840d05367d2e0ca5620b29c70
Priyatam Mudivarti <priyatam@gmail.com>
2018-12-13 00:04:11 -0500

    update deps and cleanup

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject learn-clojurescript "0.3.1-SNAPSHOT"
+(defproject learn-clojurescript "0.3.2-SNAPSHOT"
   :description "Learn Isomorphic Clojurescript on the REPL"
   :url "https://github.com/priyatam/learn-clojurescript"
   :scm {:name "git" :url "https://github.com/priyatam/learn-clojurescript"}
@@ -7,12 +7,11 @@
   :jvm-opts ["-Xmx256m" "-server"]
   :global-vars {*warn-on-reflection* false *assert* false}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.126"]
-                 [org.clojure/core.async "0.3.443"]
+                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/core.async "0.4.490"]
                  [org.clojure/core.match "0.3.0-alpha5"]
-                 [com.cognitect/transit-cljs "0.8.243"]
-                 [com.cognitect/transit-clj "0.8.300"]
-                 [com.stuartsierra/component "0.3.2"]]
+                 [cljs-http "0.1.45"]
+                 [garden "1.3.6"]]
   :cljsbuild {:builds [{:id "demos"
                         :source-paths ["demos"]
                         :compiler {:optimizations :whitespace
@@ -21,5 +20,5 @@
   :profiles {:dev  {:dependencies [[facjure/replify "0.4.1"]
                                    [garden "1.3.4"]]}}
   :plugins [[lein-cljsbuild "1.1.7"]
-            [lein-marginalia "0.8.0"]]
+            [lein-garden "0.2.8"]]
   :main replify)
